64ee3f1995e1f690bd75236937d13e7f6f998e22
akshat <akshxat@gmail.com>
2025-02-23 17:47:03 -0400

    Updated resume page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
 /*
-    Author: Akshat Bhatnagar
-    Date: 2024-12-12
-    Description: JavaScript for Akshat's E-Portfolio
+	Author: Akshat Bhatnagar
+	Date: 2024-12-12
+	Description: JavaScript for Akshat's E-Portfolio
 */
+
+const pdf = 'resume-akshat.pdf';
+
+pdfjsLib.getDocument(pdf).promise.then(pdf => {
+
+	pdf.getPage(1).then(page => {
+		const scale = 3;
+		const viewport = page.getViewport({ scale });
+
+		const canvas = document.getElementById('pdf-canvas');
+		const context = canvas.getContext('2d');
+
+		canvas.width = viewport.width;
+		canvas.height = viewport.height;
+
+		const renderContext = {
+			canvasContext: context,
+			viewport: viewport
+		};
+		page.render(renderContext);
+	});
+}).catch(error => {
+	console.error('Error loading PDF:', error);
+});
